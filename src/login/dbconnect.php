24cb3f566a650b5e7236e936bfea306caf3abbe7
hayabusakuma <mac.8create@gmail.com>
2022-06-09 17:12:15 +0900

    ログイン機能

--- src/login/dbconnect.php
+++ src/login/dbconnect.php
@@ -1,30 +1,26 @@
-<?php
-require_once './env.php';
 
-// データベース接続
-function connect(){
+<?php
+require_once 'env.php';
+
+function connect()
+{
     $host = DB_HOST;
-    $db = DB_NAME;
+    $db   = DB_NAME;
     $user = DB_USER;
     $pass = DB_PASS;
 
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
-    // エラー検知
     try {
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
-        echo '接続成功';
+        return $pdo;
     } catch(PDOExeption $e) {
-        echo '接続失敗'. $e->getMessage();
+        echo '接続失敗です！'. $e->getMessage();
         exit();
     }
 
 
 }
-
-echo connect();
-
-?>
